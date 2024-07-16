a68efd05201da88af553f36dadd67a612ce9f7f9
Heldrun <dfurlongd@gmail.com>
2024-07-16 07:56:46 +0100

    Add project files.

---
+++ Data/AlwaysLikeThisContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using AlwaysLikeThis.Models;
+
+namespace AlwaysLikeThis.Data
+{
+    public class AlwaysLikeThisContext : DbContext
+    {
+        public AlwaysLikeThisContext (DbContextOptions<AlwaysLikeThisContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<AlwaysLikeThis.Models.Article> Article { get; set; } = default!;
+    }
+}
---
+++ Models/Article.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AlwaysLikeThis.Models;
+
+public class Article
+{
+    public int Id { get; set; }
+    public string? Title { get; set; }
+    [DataType(DataType.Date)]
+    public DateTime ReleaseDate { get; set; }
+    public string? Genre { get; set; }
+    public decimal Price { get; set; }
+}
